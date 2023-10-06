e3e4df27b334c07099eaef2072b809164582ef3b
Windki <i@windki.com>
2023-10-06 12:41:20 +0800

    2023.10.6-4

--- Numbers1t9Sort2023.-10-7.cpp
+++ Numbers1t9Sort2023.-10-7.cpp
@@ -9,8 +9,8 @@ bool isExpressionValid(int* digits) {
     return (num1 + num2 == num3);
 }
 
-bool isAppeared(int *arr, int num2){
-    for(int i=0;i<337;i++){
+bool isAppeared(int *arr, int num2, int count){
+    for(int i=0;i<count;i++){
         if(arr[i]==num2){
             return 1;
         }
@@ -19,20 +19,23 @@ bool isAppeared(int *arr, int num2){
 }
 
 void generateAndOutputExpressions() {
-    int digits[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int digits[9];
+    for(int i=1;i<=9;i++){
+        digits[i-1]=i;
+    }
     int count = 0;
     int count1 = 0;
-    int arr[337] = {0};
+    int *arr = new int[count+1]();
     do{
         if (isExpressionValid(digits)) {
             int num1 = digits[0] * 100 + digits[1] * 10 + digits[2];
-            if(!isAppeared(arr,num1)){
+            if(!isAppeared(arr,num1,count)){
                 arr[count1] = num1;
                 count1++;
             }
             int num2 = digits[3] * 100 + digits[4] * 10 + digits[5];
             int num3 = digits[6] * 100 + digits[7] * 10 + digits[8];
-            if(isAppeared(arr,num2)){
+            if(isAppeared(arr,num2,count)){
                 continue;
             }
             cout << num1 << "+" << num2 << "=" << num3;
@@ -45,8 +48,8 @@ void generateAndOutputExpressions() {
             
     }
     }while(next_permutation(digits,digits+9));
-
     cout <<"Total of " << count << " Expressions."<<endl;
+    delete[] arr;
 }
 
 int main() {
