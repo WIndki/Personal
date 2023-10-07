e707e1a1849bca0baee1fea89e2138370c5e91be
Windki <i@windki.com>
2023-10-07 15:40:55 +0800

    2023.10.7

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -2,11 +2,13 @@
 #include <iomanip>
 using namespace std;
 
-template<typename I>
-void reverseArray(I array[], int size){
+template <typename I>
+void reverseArray(I array[], int size)
+{
     int left = 0;
-    int right = size -1;
-    while (left < right) {
+    int right = size - 1;
+    while (left < right)
+    {
         I temp = array[left];
         array[left] = array[right];
         array[right] = temp;
@@ -15,38 +17,46 @@ void reverseArray(I array[], int size){
     }
 }
 
+template <typename I>
+void ToOutputArray(I inputArray[],int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << inputArray[i] << " ";
+    }
+}
 
-int main(){
+template <typename I>
+void inputAndProcessArray(I inputArray[],int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cin >> inputArray[i];
+    }
+    reverseArray(inputArray, size);
+    ToOutputArray(inputArray,size);
+}
+
+int main()
+{
     int dataType;
     int size;
-    while(cin >> dataType >> size){
-        if(dataType == 1){
-            int inputArray[size-1];
-            for(int i = 0; i < size; i++){
-                cin>>inputArray[i];
-            }
-            reverseArray(inputArray,size);
-            for(int i=0;i<size;i++){
-                cout<<inputArray[i]<<" ";
-            }
-        }else if(dataType == 2){
-            long inputArray[size-1];
-            for(int i = 0; i < size; i++){
-                cin>>inputArray[i];
-            }
-            reverseArray(inputArray,size);
-            for(int i=0;i<size;i++){
-                cout<<inputArray[i]<<" ";
-            }
-        }else if(dataType == 3){
-            double inputArray[size -1];
-            for(int i = 0; i < size; i++){
-                cin>>inputArray[i];
-            }
-            reverseArray(inputArray,size);
-            for(int i=0;i<size;i++){
-                cout<<inputArray[i]<<" ";
-            }
+    while (cin >> dataType >> size)
+    {
+        if (dataType == 1)
+        {
+            int inputArray[size - 1];
+            inputAndProcessArray(inputArray,size);
+        }
+        else if (dataType == 2)
+        {
+            long inputArray[size - 1];
+            inputAndProcessArray(inputArray,size);
+        }
+        else if (dataType == 3)
+        {
+            double inputArray[size - 1];
+            inputAndProcessArray(inputArray,size);
         }
     }
     return 0;
