aa275920e39adb1aef13f0da3f34c8ae44a27d63
Windki <i@windki.com>
2023-10-22 18:38:35 +0800

    2023.10.22

--- matrixmulti.cpp
+++ matrixmulti.cpp
@@ -31,30 +31,30 @@ public:
             cout << endl;
         }
     }
-    // Matrix operator*(const Matrix& B) const { //重载运算符函数
-    //     Matrix C(row, B.col);
-    //     for (int i = 0; i < row; i++) {  //矩阵乘法 复杂度O(n^3)
-    //         for (int j = 0; j < B.col; j++) {
-    //             for (int k = 0; k < col; k++) {
-    //                 C.data[i][j] += data[i][k] * B.data[k][j];
-    //             }
-    //         }
-    //     }
-    //     return C;
-    // }
-};
-
-Matrix multiMatrix(const Matrix A, const Matrix B) {
-    Matrix C(A.row, B.col);
-    for (int i=0;i<A.row;i++){
-        for (int j=0;j<B.col;j++){
-            for (int k=0;k<A.col;k++){
-                C.data[i][j]+=A.data[i][k]*B.data[k][j];
+    Matrix operator*(const Matrix& B) const { //重载运算符函数
+        Matrix C(row, B.col);
+        for (int i = 0; i < row; i++) {  //矩阵乘法 复杂度O(n^3)
+            for (int j = 0; j < B.col; j++) {
+                for (int k = 0; k < col; k++) {
+                    C.data[i][j] += data[i][k] * B.data[k][j];
+                }
             }
         }
+        return C;
     }
-    return C;
-}
+};
+
+// Matrix multiMatrix(const Matrix A, const Matrix B) {
+//     Matrix C(A.row, B.col);
+//     for (int i=0;i<A.row;i++){
+//         for (int j=0;j<B.col;j++){
+//             for (int k=0;k<A.col;k++){
+//                 C.data[i][j]+=A.data[i][k]*B.data[k][j];
+//             }
+//         }
+//     }
+//     return C;
+// }
 
 int main() {
     int r1, c1, r2, c2;
@@ -64,7 +64,7 @@ int main() {
         cin >> r2 >> c2;
         Matrix B(r2, c2);
         B.input();
-        Matrix C = multiMatrix(A, B);
+        Matrix C = A*B;
         C.output();
     }
     return 0;
