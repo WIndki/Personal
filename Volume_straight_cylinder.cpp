8e84d273357ac373a7e938a5049484e97c5eafac
Windki <i@windki.com>
2023-11-04 12:49:52 +0800

    2023.11.4

---
+++ Volume_straight_cylinder.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+class straight_cylinder
+{
+private:
+    double area;
+    double height;
+    double volume;
+public:
+    straight_cylinder(double a, double h)
+    {
+        area = a;
+        height = h;
+        volume = a*h;
+    }
+    double get_volume()
+    {
+        return volume;
+    }
+};
+
+int main(){
+    int op;
+    while(cin>>op){
+        if(op==0){
+            double radius;
+            double h=0;
+            cin>>radius>>h;
+            double radius_area = M_PI*radius*radius;
+            straight_cylinder sc(radius_area, h);
+            cout<<fixed<<setprecision(2)<<sc.get_volume()<<endl;
+        }
+        else if(op==1){
+            double edge1,edge2,edge3;
+            double h=0;
+            cin>>edge1>>edge2>>edge3>>h;
+            double p = (edge1+edge2+edge3) / 2; 
+            double area = sqrt(p * (p - edge1) * (p - edge2) * (p - edge3));
+            straight_cylinder sc2(area, h);
+            cout<<fixed<<setprecision(2)<<sc2.get_volume()<<endl;
+        }
+        else if(op==2){
+            double length, width;
+            double h=0;
+            cin>>length>>width>>h;
+            double area2 = length*width;
+            straight_cylinder sc3(area2, h);
+            cout<<fixed<<setprecision(2)<<sc3.get_volume()<<endl;
+        }
+    }
+    return 0;
+}
---
+++ complex_number.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+template<class T>
+class complex_numbers
+{
+private:
+    T real;
+    T imaginary;
+public:
+    complex_numbers(T r = 0, T i = 0)
+    {
+        real = r;
+        imaginary = i;
+    }
+    ~complex_numbers()
+    {
+        real = 0;
+        imaginary = 0;
+    }
+    complex_numbers operator+(complex_numbers const &obj)
+    {
+        complex_numbers res;
+        res.real = real + obj.real;
+        res.imaginary = imaginary + obj.imaginary;
+        return res;
+    }
+    complex_numbers operator-(complex_numbers const &obj)
+    {
+        complex_numbers res;
+        res.real = real - obj.real;
+        res.imaginary = imaginary - obj.imaginary;
+        return res;
+    }
+    complex_numbers operator*(complex_numbers const &obj)
+    {
+        complex_numbers res;
+        res.real = real * obj.real - imaginary * obj.imaginary;
+        res.imaginary = real * obj.imaginary + imaginary * obj.real;
+        return res;
+    }
+    complex_numbers operator/(complex_numbers const &obj)
+    {
+        complex_numbers res;
+        res.real = (real * obj.real + imaginary * obj.imaginary) / (obj.real * obj.real + obj.imaginary * obj.imaginary);
+        res.imaginary = (imaginary * obj.real - real * obj.imaginary) / (obj.real * obj.real + obj.imaginary * obj.imaginary);
+        return res;
+    }
+    void print()
+    {
+        cout <<"("<<fixed<<setprecision(2)<<real <<","<<fixed<<setprecision(2)<<imaginary <<"i)"<< endl;
+    }
+};
+
+int main()
+{
+    int n;
+    while (cin >> n)
+    {
+        int op=0;
+        complex_numbers<float> c;
+        for(int count=0;count<n;count++)
+        {
+            float r,i;
+            cin >> r >> i;
+            complex_numbers<float> a(r,i);
+            if(count==0)c=a;
+            if(op==1){
+                c=c+a;
+            }
+            else if(op==2){
+                c=c-a;
+            }
+            else if(op==3){
+                c=c*a;
+            }
+            else if(op==4){
+                c=c/a;
+            }
+            if(count<n-1)cin>>op;
+        }
+        c.print();
+    }
+    return 0;
+}
+
