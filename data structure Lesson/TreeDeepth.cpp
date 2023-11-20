eb21853eb874fb13b32da47b0319cf60dcff4b9e
Windki <i@windki.com>
2023-11-20 19:03:52 +0800

    2023.11.20

--- data structure Lesson/TreeDeepth.cpp
+++ data structure Lesson/TreeDeepth.cpp
@@ -2,103 +2,119 @@
 #include <queue>
 using namespace std;
 
-struct BiTNode {
+struct BiTNode
+{
     char data;
     BiTNode *lchild, *rchild;
-    BiTNode() : data(), lchild(nullptr), rchild(nullptr) {};
+    BiTNode(char x) : data(x), lchild(nullptr), rchild(nullptr){};
 };
 
-void CreateBiTree(BiTNode *&root){
+void CreateBiTree(BiTNode *&root)
+{
     char parent, left, right;
     queue<BiTNode *> Q;
     cin >> parent;
-    if (parent == '#') return;
-    root = new BiTNode();
-    root->data = parent;
-    root->lchild = root->rchild = NULL;
+    if (parent == '#')
+        return;
+    root = new BiTNode(parent);
     Q.push(root);
-    while(cin>>parent){
-        if(parent=='#')break;
+    while (cin >> parent)
+    {
+        if (parent == '#')
+            break;
         BiTNode *BT = Q.front();
         Q.pop();
         cin >> left;
-        if(left == '0'){
+        if (left == '0')
+        {
             BT->lchild = NULL;
-        }else{
-            BT->lchild = new BiTNode();
-            BT->lchild->data = left;
-            BT->lchild->lchild = BT->lchild->rchild = NULL;
+        }
+        else
+        {
+            BT->lchild = new BiTNode(left);
             Q.push(BT->lchild);
         }
         cin >> right;
-        if(right == '0'){
+        if (right == '0')
+        {
             BT->rchild = NULL;
-        }else{
-            BT->rchild = new BiTNode();
-            BT->rchild->data = right;
-            BT->rchild->lchild = BT->rchild->rchild = NULL;
+        }
+        else
+        {
+            BT->rchild = new BiTNode(right);
             Q.push(BT->rchild);
         }
     }
 }
 
-void swapChildren(BiTNode *&root) {
-    if (root == nullptr) {
+void swapChildren(BiTNode *&root)
+{
+    if (root == nullptr)
+    {
         return;
     }
-    BiTNode* temp = root->lchild;
+    BiTNode *temp = root->lchild;
     root->lchild = root->rchild;
     root->rchild = temp;
     swapChildren(root->lchild);
     swapChildren(root->rchild);
 }
 
-void levelOrderTraversal(BiTNode* root) {
-    if (root == nullptr) {
+void levelOrderTraversal(BiTNode *root)
+{
+    if (root == nullptr)
+    {
         return;
     }
-    queue<BiTNode*> q;
+    queue<BiTNode *> q;
     q.push(root);
-    while (!q.empty()) {
-        BiTNode* cur = q.front();
+    while (!q.empty())
+    {
+        BiTNode *cur = q.front();
         q.pop();
         cout << cur->data << " ";
-        if(cur->lchild!=nullptr){
+        if (cur->lchild != nullptr)
+        {
+            q.push(cur->lchild);
             cout << cur->lchild->data << " ";
-        }else{
+        }
+        else
+        {
             cout << "0 ";
         }
-        if(cur->rchild!=nullptr){
+        if (cur->rchild != nullptr)
+        {
+            q.push(cur->rchild);
             cout << cur->rchild->data << endl;
-        }else{
+        }
+        else
+        {
             cout << "0" << endl;
         }
-        if (cur->lchild != nullptr) {
-            q.push(cur->lchild);
-        }
-        if (cur->rchild != nullptr) {
-            q.push(cur->rchild);
-        }
     }
 }
 
-int TreeDeep(BiTNode *root){
-    if(root==nullptr){
+int TreeDeep(BiTNode *root)
+{
+    if (root == nullptr)
+    {
         return 0;
-    }else{
+    }
+    else
+    {
         int l = TreeDeep(root->lchild);
         int r = TreeDeep(root->rchild);
-        int deepth = max((l+1),(r+1));
+        int deepth = max((l + 1), (r + 1));
         return deepth;
     }
-
 }
 
-int main(){
+int main()
+{
     BiTNode *T;
     CreateBiTree(T);
-    //swapChildren(T);
-    //levelOrderTraversal(T);
+    // swapChildren(T);
+    // levelOrderTraversal(T);
     cout << TreeDeep(T) << endl;
     return 0;
 }
--- data structure Lesson/sumOfleavesNode.cpp
+++ data structure Lesson/sumOfleavesNode.cpp
@@ -1,36 +1,42 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-struct BiTNode {
+struct BiTNode
+{
     char data;
-    BiTNode* left,* right;
-    BiTNode(char x) : data(x), left(nullptr), right(nullptr) {};
+    BiTNode *left, *right;
+    BiTNode(char x) : data(x), left(nullptr), right(nullptr){};
     BiTNode() : data(), left(nullptr), right(nullptr) {}
 };
 
-BiTNode* createBinaryTree(char values[],int size) {
+BiTNode *createBinaryTree(char values[], int size)
+{
     // if (values[0]=='0') {
     //     return nullptr;
     // }
 
-    BiTNode* root = new BiTNode(values[0]);
-    queue<BiTNode*> q;
+    BiTNode *root = new BiTNode(values[0]);
+    queue<BiTNode *> q;
     q.push(root);
 
     int i = 1;
-    while (!q.empty() && i < size) {
-        BiTNode* curr = q.front();
+    while (!q.empty() && i < size)
+    {
+        BiTNode *curr = q.front();
         q.pop();
 
         int leftVal = values[i++];
-        if (leftVal != '0') {
+        if (leftVal != '0')
+        {
             curr->left = new BiTNode(leftVal);
             q.push(curr->left);
         }
 
-        if (i < size) {
+        if (i < size)
+        {
             int rightVal = values[i++];
-            if (rightVal != '0') {
+            if (rightVal != '0')
+            {
                 curr->right = new BiTNode(rightVal);
                 q.push(curr->right);
             }
@@ -40,27 +46,33 @@ BiTNode* createBinaryTree(char values[],int size) {
     return root;
 }
 
-void createFullBT_DFS(BiTNode *root, char numbers[], int len, int i) {
-    if(i <= len) {
+void createFullBT_DFS(BiTNode *root, char numbers[], int len, int i)
+{
+    if (i <= len)
+    {
         root->data = numbers[i - 1];
-        if(2 * i <= len && numbers[2 * i - 1] != '0') {
+        if (2 * i <= len && numbers[2 * i - 1] != '0')
+        {
             root->left = new BiTNode();
             createFullBT_DFS(root->left, numbers, len, 2 * i);
         }
-        if((2 * i + 1) <= len && numbers[2 * i] != '0') {
+        if ((2 * i + 1) <= len && numbers[2 * i] != '0')
+        {
             root->right = new BiTNode();
             createFullBT_DFS(root->right, numbers, len, 2 * i + 1);
         }
     }
 }
 
-
-int countLeaves(BiTNode* root) {
-    if (root == nullptr) {
+int countLeaves(BiTNode *root)
+{
+    if (root == nullptr)
+    {
         return 0;
     }
 
-    if (root->left == nullptr && root->right == nullptr) {
+    if (root->left == nullptr && root->right == nullptr)
+    {
         return 1;
     }
 
@@ -70,39 +82,48 @@ int countLeaves(BiTNode* root) {
     return leftLeaves + rightLeaves;
 }
 
-bool compare(BiTNode *root1, BiTNode *root2) {
-    if (root1 == nullptr && root2 == nullptr) {
+bool compare(BiTNode *root1, BiTNode *root2)
+{
+    if (root1 == nullptr && root2 == nullptr)
+    {
         return true;
     }
 
-    if (root1 == nullptr || root2 == nullptr) {
+    if (root1 == nullptr || root2 == nullptr)
+    {
         return false;
     }
 
-    if (root1->data != root2->data) {
+    if (root1->data != root2->data)
+    {
         return false;
     }
 
     return compare(root1->left, root2->left) && compare(root1->right, root2->right);
 }
 
-int main() {
+int main()
+{
     char input[100];
-    int nums=0;
-    for(int i=0;i<100;i++){
+    int nums = 0;
+    for (int i = 0; i < 100; i++)
+    {
         char temp;
-        cin>>temp;
-        if(temp=='#')
+        cin >> temp;
+        if (temp == '#')
         {
-            nums=i;
+            nums = i;
             break;
-        }else input[i]=temp;
+        }
+        else
+            input[i] = temp;
     }
     char values[nums];
-    for(int i=0;i<nums;i++){
-        values[i]=input[i];
+    for (int i = 0; i < nums; i++)
+    {
+        values[i] = input[i];
     }
-    BiTNode* root1 = createBinaryTree(values,nums);
+    BiTNode *root1 = createBinaryTree(values, nums);
     int leaves1 = countLeaves(root1);
     cout << leaves1 << endl;
     BiTNode *root2 = new BiTNode();
